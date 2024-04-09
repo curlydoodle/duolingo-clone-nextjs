abfb23e16c709b7425bbde9d179e3df4ad970ebd
Kyaw Thu <evanch98@gmail.com>
2024-04-09 16:58:24 +0800

    update the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
       <Button>Default</Button>
       <Button variant="primary">Primary</Button>
-      <Button>Primary Outline</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
     </div>
   );
 }
