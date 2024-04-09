7728961e5e1b85b06397dcc2b55408c8f17efd6d
Kyaw Thu <evanch98@gmail.com>
2024-04-09 16:54:32 +0800

    update the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+      <Button>Default</Button>
       <Button>Primary</Button>
       <Button>Primary Outline</Button>
     </div>
