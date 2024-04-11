7d9d1da2514912562a2daf5913a15e2dc69ca8b8
Kyaw Thu <evanch98@gmail.com>
2024-04-11 20:41:59 +0800

    update the MarketingLayout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,7 +3,13 @@ type Props = {
 };
 
 const MarketingLayout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default MarketingLayout;
